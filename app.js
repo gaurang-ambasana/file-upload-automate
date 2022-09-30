dec81827a1f9132084edfe9b681afecb4ea6cf15
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-30 22:04:30 +0530

    organized + helpers

--- app.js
+++ app.js
@@ -15,23 +15,29 @@ const googleDrive = google.drive({ version: "v3", auth: authCli });
 
 const query = `mimeType: 'text/csv'`;
 
+const fileIds = [];
+
 do {
-  googleDrive.files.list(
-    {
-      corpora,
-      driveId,
-      supportsAllDrives,
-      includeItemsFromAllDrives,
-      q: query,
-      pageToken,
-    },
-    (err, res) => {
-      if (err) console.error(err.stack);
-      else {
-        const { files, nextPageToken } = res.data;
-        pageToken = nextPageToken;
-        console.log(files);
-      }
-    }
-  );
+  const {
+    data: { files },
+  } = await googleDrive.files.list({
+    corpora,
+    driveId,
+    supportsAllDrives,
+    includeItemsFromAllDrives,
+    q: query,
+    pageToken,
+  });
+  fileIds.push(...files.map(({ id }) => id));
 } while (pageToken);
+
+for (let i = 0, n = fileIds.length; i < n; i++) {
+  const { data } = await googleDrive.files.get({
+    fileId: fileIds[i],
+    supportsAllDrives,
+    alt: "media",
+  });
+
+  const blob = new Blob([data]);
+  console.log(blob);
+}
