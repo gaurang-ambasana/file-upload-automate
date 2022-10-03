0e19ba37b43860467972c1b8c4a4354d678e9fe8
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-03 17:08:38 +0530

    added upload functionality

---
+++ upload/helpers/helpers.js
@@ -0,0 +1,30 @@
+import { format } from "util";
+import { config } from "dotenv";
+import storage from "../config/index.js";
+
+config();
+
+const csvBucket = storage.bucket(process.env.BUCKET_NAME);
+
+export const uploadCsv = (file, name, mimeType) =>
+  new Promise((resolve, reject) => {
+    const { buffer } = file;
+
+    const blob = csvBucket.file(name.replace(/ /g, "_"));
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+      contentType: mimeType,
+    });
+    blobStream
+      .on("finish", () => {
+        //   const publicUrl = format(
+        //     `https://storage.googleapis.com/${csvBucket.name}/${blob.name}`
+        //   );
+        resolve("Upload Success!");
+      })
+      .on("error", (err) => {
+        console.error(err.message);
+        reject(`Unable to upload, something went wrong`);
+      })
+      .end(buffer);
+  });
