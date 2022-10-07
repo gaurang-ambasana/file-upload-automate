1f5c1ed145409bb2f82c3532cb998df9721d88a2
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-07 15:37:09 +0530

    all workflow in single route-server

--- app.js
+++ app.js
@@ -1,61 +1,63 @@
+import express from "express";
+import bodyParser from "body-parser";
 import { config } from "dotenv";
-import { google } from "googleapis";
-import authCli from "./auth/auth.js";
+import {
+  getFileContent,
+  listCsv,
+  uploadCsv,
+} from "./upload/helpers/helpers.js";
 
 config();
 
-const corpora = `drive`;
-const driveId = process.env.DRIVE_ID;
-const supportsAllDrives = true;
-const includeItemsFromAllDrives = true;
+const { PORT } = process.env;
+const app = express();
 
-let pageToken;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const googleDrive = google.drive({ version: "v3", auth: authCli });
-
-const query = `mimeType: 'text/csv'`;
-
-const allCsv = [];
-
-do {
-  const {
-    data: { files },
-  } = await googleDrive.files.list({
-    corpora,
-    driveId,
-    supportsAllDrives,
-    includeItemsFromAllDrives,
-    q: query,
-    pageToken,
-  });
-  allCsv.push(
-    ...files.map(({ id, name, mimeType }) => ({ id, name, mimeType }))
-  );
-} while (pageToken);
-
-allCsv.forEach(async ({ id: fileId, mimeType, name: fileName }) => {
+app.get("/fetch-csv-and-upload", async ({ query: { driveId } }, res, next) => {
   try {
-    const { data } = await googleDrive.files.get({
-      fileId,
-      supportsAllDrives,
-      alt: "media",
+    const payload = [];
+    const allCsv = await listCsv(driveId);
+
+    for (let i = 0, n = allCsv.length; i < n; i++) {
+      const { id: fileId, mimeType, name: fileName } = allCsv[i];
+
+      const { data } = await getFileContent(fileId);
+
+      const file = await new Blob([data]).arrayBuffer();
+
+      const { msg, publicUrl } = await uploadCsv(file, fileName, mimeType);
+
+      if (publicUrl) {
+        console.log(`"${fileName}" uploaded successfully.`);
+        payload.push({ name: fileName, msg, publicUrl });
+      } else {
+        console.log(`"${fileName}" upload failed.`);
+        payload.push({ name: fileName, err: msg });
+      }
+    }
+
+    res.status(200).json({
+      status: "OK!",
+      payload,
     });
-
-    const blob = new Blob([data]);
-
-    const formData = new FormData();
-    formData.append("file", blob);
-    formData.append("name", fileName);
-    formData.append("mimeType", mimeType);
-
-    const { status, msg, publicUrl } = await fetch(process.env.UPLOAD_URL, {
-      method: "POST",
-      body: formData,
-    });
-
-    if (status === 200) console.log(`"${fileName}" uploaded successfully`);
-    else console.error(`"${fileName}" upload failed, ${msg}`);
   } catch (e) {
     console.error(e);
+    next(e);
   }
 });
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    erorr: err,
+    msg: "Internal Server Error!",
+  });
+  next();
+});
+
+app.get("/", (req, res) => res.status(200).send("API is running..."));
+
+app.listen(PORT, () =>
+  console.log(`Server is listening to all the requests from PORT: ${PORT}`)
+);
